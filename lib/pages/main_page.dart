53f6366050fefd957806c1daefed748bf4e9341b
jazmin <correo@example>
2022-12-25 03:38:39 -0500

    actualizando

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -11,6 +11,7 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +20,33 @@ class _MainPageState extends State<MainPage> {
         child: Text(
           'MainPage',
           style: TextStyle(color: green, fontSize: 32),
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(
+              icons.length,
+              (index) => Container(
+                    height: 40,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          width: 24,
+                          height: 3,
+                          decoration: BoxDecoration(
+                              color: currentPage == index ? green : white,
+                              borderRadius: BorderRadius.circular(5)),
+                        ),
+                        Icon(
+                          icons[index],
+                          color: currentPage == index ? green : white,
+                        ),
+                      ],
+                    ),
+                  )),
         ),
       ),
     );
