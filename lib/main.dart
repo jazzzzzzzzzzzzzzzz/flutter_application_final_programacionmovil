52157b07a8aa4862e8f5a6f0a217d45c9f4b856d
jazmin <correo@example>
2022-12-25 03:24:07 -0500

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/main_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +26,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MainPage(),
     );
   }
 }
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_final_programacionmovil/const.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: black,
+      body: Center(
+        child: Text(
+          'MainPage',
+          style: TextStyle(color: green, fontSize: 32),
+        ),
+      ),
+    );
+  }
+}
